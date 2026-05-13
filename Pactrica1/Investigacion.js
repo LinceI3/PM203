a01f73176ae15f69233366ca5e9179cfc736b346
Armando <armando.yael.contreras.13@gmail.com>
2026-05-13 17:56:24 -0600

    “mensaje”

---
+++ Pactrica1/Investigacion.js
@@ -0,0 +1,28 @@
+function Menu(){
+    let cafe = "Cafe Americano ";
+    let latte = "Latte";
+    let  frappe = "Frappe";
+
+    console.log(`
+
+================================
+
+      CAFETERÍA UPQ
+
+================================
+
+1. ${cafe} .......... $45
+
+2. ${latte} .......... $65
+
+3. ${frappe} ......... $80
+
+4. Salir
+
+================================
+
+`);
+
+}
+
+Menu();
